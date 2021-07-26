4390cff8b1b33e4acd2a4dbef841112ac6ed1d05
vaibhavvk9611 <vaibhavvk9611@gmail.com>
2021-07-26 13:01:02 +0530

    app1

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About() {
+  return (
+
+    <div>
+      <h1> Vaibhav vk</h1>
+      <p>
+        My self Vaibhav vk im student at presidency University , 
+        this is my first react app 
+        Thank you
+      </p>
+      </div>
+  );
+}
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+function Home() {
+    return (
+        <div>
+          <img
+              src="https://scontent.fixe1-1.fna.fbcdn.net/v/t1.6435-9/128627664_680703182809331_731822567053136094_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=mUFlC80Gaj0AX__xVOI&_nc_oc=AQmX8iNY3S7DmCTIMPTykqoKW1L9kQ5r2lBZXyoCBq9BhW3PtRzUgdtqq2PAnt5prOE&_nc_ht=scontent.fixe1-1.fna&oh=57a6c9d0ffecaeeec108cb8bc75cac09&oe=611F972F"
+              alt=""
+            />
+         <div><h1>Age:20</h1>
+               <h1>Name:vaibhav vk</h1></div>
+            </div>
+            );
+  }
+  
+export default Home;
